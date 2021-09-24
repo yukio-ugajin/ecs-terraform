89dce3de38b9c04e6c6ed90dbf5dc4761b74fdf9
yukio-ugajin <yukio-ugajin@c-fo.com>
2021-09-24 14:47:58 +0900

    load balancerの追加

---
+++ lb.tf
@@ -0,0 +1,27 @@
+resource "aws_lb" "ugajin_lb" {
+  name               = "ugajin-test"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = module.vpc.public_subnets
+  security_groups    = [
+    aws_security_group.ecs_sg.id
+  ]
+}
+
+resource "aws_lb_listener" "ugajin_lb_listener" {
+  load_balancer_arn = aws_lb.ugajin_lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ugajin_lb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group" "ugajin_lb_target_group" {
+  name     = "ugajin-test"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
+}
