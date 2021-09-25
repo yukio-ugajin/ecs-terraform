5de1870807bf87732ca7999dfa6ab08e8bb710d1
yukio-ugajin <yukio-ugajin@c-fo.com>
2021-09-25 17:37:42 +0900

    セキュリティグループを整理

--- sg.tf
+++ sg.tf
@@ -1,36 +1,54 @@
-resource "aws_security_group" "ecs_sg" {
-  name   = "ugajin-ecs-sg"
+resource "aws_security_group" "ec2_sg" {
+  name   = "ugajin-ec2-test"
   vpc_id = module.vpc.vpc_id
 
   tags = {
-    name = "ugajin-test"
+    Name = "ugajin-test"
   }
 }
 
-resource "aws_security_group_rule" "ecs_sg_inbound_rule_for_http" {
+resource "aws_security_group_rule" "ec2_sg" {
   type              = "ingress"
   from_port         = 80
   to_port           = 80
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ecs_sg.id
+  self              = true
+  security_group_id = aws_security_group.ec2_sg.id
 }
 
-# resource "aws_security_group_rule" "ecs_sg_inbound_rule_for_ssh" {
-#   type              = "ingress"
-#   from_port         = 22
-#   to_port           = 22
-#   protocol          = "tcp"
-#   cidr_blocks       = ["0.0.0.0/0"]
-#   security_group_id = aws_security_group.ecs_sg.id
-# }
-
 # Terraformではアウトバウンドのルールも明記する必要がある。マネコン上だと自動生成される。
-resource "aws_security_group_rule" "ecs_sg_outbound_rule" {
+resource "aws_security_group_rule" "ec2_sg1" {
   type              = "egress"
   from_port         = 0
   to_port           = 65536
   protocol          = "-1" # TCP/UDP両方の指定
   cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.ecs_sg.id
+  security_group_id = aws_security_group.ec2_sg.id
+}
+
+resource "aws_security_group" "lb_sg" {
+  name   = "ugajin-test"
+  vpc_id = module.vpc.vpc_id
+
+  tags = {
+    Name = "ugajin-test"
+  }
+}
+
+resource "aws_security_group_rule" "lb_sg" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_sg.id
+}
+
+resource "aws_security_group_rule" "lb_sg1" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65536
+  protocol          = "-1" # TCP/UDP両方の指定
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.lb_sg.id
 }
