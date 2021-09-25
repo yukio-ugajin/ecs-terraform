8618b47046a526ab1a900bb57da5140ef68b492c
yukio-ugajin <yukio-ugajin@c-fo.com>
2021-09-26 00:05:14 +0900

    ヘルスチェックの設定を明示

--- lb.tf
+++ lb.tf
@@ -22,4 +22,10 @@ resource "aws_lb_target_group" "ugajin_lb_target_group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = module.vpc.vpc_id
+
+  health_check {
+    interval = 20
+    timeout  = 10
+    port = "traffic-port"
+  }
 }
